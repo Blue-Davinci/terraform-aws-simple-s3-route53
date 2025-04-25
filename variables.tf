518a7280176b65f0e47b21988555ae7610d90663
DaVinCi <52623427+Blue-Davinci@users.noreply.github.com>
2025-04-25 20:16:51 +0300

    test commit

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "region" {
-    description = "value of the region to deploy the resources"
-    type = string
-    default = "us-east-1"
+  description = "value of the region to deploy the resources"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "domain_name" {
-    description = "Domain name for the hosted zone"
-    type = string
+  description = "Domain name for the hosted zone"
+  type        = string
 }
